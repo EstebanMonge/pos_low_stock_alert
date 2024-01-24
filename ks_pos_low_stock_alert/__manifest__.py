aa3aa144f675d05fa7a358692864119ef7f09770
Esteban Monge <estebanmonge@riseup.net>
2024-01-23 22:31:32 -0600

    Add a popup to alert about low stock products

--- ks_pos_low_stock_alert/__manifest__.py
+++ ks_pos_low_stock_alert/__manifest__.py
@@ -4,28 +4,28 @@
     'summary': """Manage the Stock of your POS products by highlighting them in different colors depending on
                 their stock state.""",
     'description': """
-        POS Low Stock Alert v11.0
+        POS Low Stock Alert v15.0
         Manage the Stock of your POS products by highlighting them in different colors depending on
                 their stock state.
-        POS
-        POS Low Stock Alert
-        Low Stock Alert
-        Stock Alert
-        POS Low Stock Warning
-        POS Stock Warning
-        Inventory Alert
-        Inventory Minimum Quantity Alert
-        Inventory Minimum Quantity Warning
-        POS Manager
-        POS Inventory
-        POS Stock
-        POS Stock Alert
-        POS Retail
-        POS Shop
-        Point of Sales
-        Point of Sales Stock Alert
-        Point of Sales Low Stock Alert
-        POS Shop Low Stock
+        POS,
+        POS Low Stock Alert,
+        Low Stock Alert,
+        Stock Alert,
+        POS Low Stock Warning,
+        POS Stock Warning,
+        Inventory Alert,
+        Inventory Minimum Quantity Alert,
+        Inventory Minimum Quantity Warning,
+        POS Manager,
+        POS Inventory,
+        POS Stock,
+        POS Stock Alert,
+        POS Retail,
+        POS Shop,
+        Point of Sales,
+        Point of Sales Stock Alert,
+        Point of Sales Low Stock Alert,
+        POS Shop Low Stock,
         stock notification, 
         low stock notification, 
         product stock notification, 
@@ -40,17 +40,31 @@
         odoo website product stock, 
         odoo website stock management
     """,
-    'author': 'Ksolves India Pvt. Ltd.',
+    'author': 'Ksolves India Ltd.',
     'website': "https://www.ksolves.com/",
     'license': 'LGPL-3',
+    'currency': 'EUR',
+    'price': '0.0',
     'category': 'Point Of Sale',
     'support': 'sales@ksolves.com',
-    'version': '1.0.0',
-    'images': ['static/description/main.jpg'],
+    'version': '15.0.1.0.0',
+    'images': ['static/description/pos_15.jpg'],
     'depends': ['point_of_sale'],
     'data': [
-        'views/assets.xml',
+        # 'views/assets.xml',
         'views/config.xml'
     ],
-    'qweb': ['static/src/xml/ks_low_stock.xml']
+    # 'qweb': ['static/src/xml/ks_low_stock.xml']
+    'assets' : {
+        'point_of_sale.assets': [
+            'ks_pos_low_stock_alert/static/src/css/ks_low_stock.css',
+            'ks_pos_low_stock_alert/static/src/js/ks_utils.js',
+            'ks_pos_low_stock_alert/static/src/js/ks_low_stock.js',
+            'ks_pos_low_stock_alert/static/src/js/ks_product_list.js',
+            'ks_pos_low_stock_alert/static/src/js/ks_product_screen.js',
+        ],
+        'web.assets_qweb': [
+            'ks_pos_low_stock_alert/static/src/xml/**/*',
+        ]
+    }
 }
--- ks_pos_low_stock_alert/models/ks_pos_config.py
+++ ks_pos_low_stock_alert/models/ks_pos_config.py
@@ -10,5 +10,6 @@ class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     display_stock = fields.Boolean(string = 'Display Stock of products in POS', default = True)
+    display_popup = fields.Boolean(string = 'Display Popup when products are lower than Minimum Limit', default = True)
     minimum_stock_alert = fields.Integer(string='Minimum Limit to change the stock color for the product', default = 0)
     allow_order_when_product_out_of_stock = fields.Boolean(string = 'Allow Order when Product is Out Of Stock', default = True)
